a5089480a736aef6570e2cfd4939f7bed06edc6a
HEOYONGHOI <hyhh1997@naver.com>
2017-05-16 10:49:54 +0000

    second commit

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,14 @@
 class Article < ApplicationRecord
-  def self.news_title
+  def self.news_title(search)
     agent = Mechanize.new
-    [1,11,21,31].each do |index|
-      page = agent.get"https://search.naver.com/search.naver?sm=tab_hty.top&where=news&ie=utf8&query=멋쟁이사자처럼&start=#{index}"
+    array = params[:@title]
+    array.each do |index|
+      page = agent.get"https://search.naver.com/search.naver?sm=tab_hty.top&where=news&ie=utf8&query=#{search}&start=#{index}"
       list = page.search("a._sp_each_title").map(&:text)
+      # amount = page.search("div.title_desc all_my span")
+      # puts amount
       list.each do |title|
-        puts title
+        articles = Article.create(title: "#{title}")
       end
     end
   end
